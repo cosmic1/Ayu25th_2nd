e98866fee85497648698310c0a80a8f855e5f714
Ken Imai <ken@cosmiclab.jp>
2023-09-18 13:16:43 +0900

    Changed LFS path.

--- SetupLfs.bat
+++ SetupLfs.bat
@@ -1,12 +1,12 @@
-set dropboxPath="%CosmicProjects%"
-set projectName=231002_Ayu_25th_Season2
+set dropboxPath="%CosmicProjects%/.."
+set projectName=231002_Ayu25th_v2
 
 call git config --unset-all lfs.customtransfer.lfs-folder.path
 call git config --unset-all lfs.customtransfer.lfs-folder.args
 call git config --unset-all lfs.standalonetransferagent
 
 call git config --add lfs.customtransfer.lfs-folder.path lfs-folderstore
-call git config --add lfs.customtransfer.lfs-folder.args "Y:/Dropbox/LFS_for_AYU/LFS"
+call git config --add lfs.customtransfer.lfs-folder.args "%dropboxPath%/COSMIC_shared/%projectName%/LFS"
 call git config --add lfs.standalonetransferagent lfs-folder
 
 call git reset --hard master
